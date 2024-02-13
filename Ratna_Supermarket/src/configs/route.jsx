9e5b3e85c2d1f37fb8533107386f35dbcf126479
Minav Karia <minavpkaria@gmail.com>
2024-02-13 15:12:15 +0530

    Add changes

---
+++ Ratna_Supermarket/src/configs/route.jsx
@@ -0,0 +1,28 @@
+import Layout from "../pages/Layout";
+import Home from "../pages/Home";
+import {createBrowserRouter,RouterProvider,Route,Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Signup from "../pages/Signup";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navbar/>
+          <Home/>
+          <Footer/>
+        </>
+      )
+    },{
+      path:"/sign",
+      element:(
+        <>
+          <Signup/>
+        </>
+      )
+    }
+]);
+
+export default router;
