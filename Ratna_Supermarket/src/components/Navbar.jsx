c4f9f6c11ed677e77b7ae36d580dffaafca33a32
Minav Karia <minavpkaria@gmail.com>
2024-02-13 15:28:46 +0530

    Add navbar

--- Ratna_Supermarket/src/components/Navbar.jsx
+++ Ratna_Supermarket/src/components/Navbar.jsx
@@ -1,9 +1,47 @@
+import React, { useState } from 'react';
+import '../assets/styles/Navbar.css';
+import { Link } from 'react-router-dom';
 
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-function Navbar() {
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar" style={{
+      position: 'fixed',
+      top: '0',
+      width: '100%',
+      zIndex: '100',
+      backgroundColor: 'white',
+      boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.2)',
+    }}>
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo" style={{ marginLeft: '50px' }}>
+          <p><span style={{ fontSize: 'xxx-large' }}>Gmart</span> </p>
+        </Link>
+        <div className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <ul className={`nav-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+          <li className="nav-item">
+            <Link to="/text" className="nav-link" onClick={toggleMobileMenu}>
+              Text AI
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/image" className="nav-link" onClick={toggleMobileMenu}>
+              Image AI
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
