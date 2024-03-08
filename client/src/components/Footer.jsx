a1e6bb6605d6f5456349b33fef71be37a67c3d60
Code-Ph0enix <eeshanya.j@somaiya.edu>
2024-03-08 17:54:52 +0530

    FOOTER CHANGES

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,13 +2,13 @@
 function Footer() {
   return (
     <>
-      <div className="w-full bg-slate-200 p-4 shadow-md mt-10">
+      <div className="w-full bg-slate-200 p-4 shadow-md mt-10 px-10">
       <div class="flex flex-row gap-[5vw] container justify-start items-center mx-auto ">
-        <div class="flex flex-col gap-2">
+        <div class="flex flex-col gap-2 items-start justify-items-start w-full">
             <p class="text-1xl">Download GMart ready for Mobile App Now!!</p>
     
             <div class="flex gap-4">
-                <img class="h-16" src="./gpp.png" alt="Google Play Logo"/>
+                <img class="h-16 flex -m-2" src="./gpp.png" alt="Google Play Logo"/>
                 <img class="h-11 my-auto" src="./gpp4.png" alt="App Store Logo"/>
             </div>
     
@@ -19,22 +19,25 @@ function Footer() {
                 <img src="rupee_gpp.png" class="h-5"/>
             </div>
         </div>
-        <div class="flex flex-col gap-[0vh]">
-            <p>FAQs</p>
-            <p>Privacy Policy</p>
-            <p>Pricing, Delivery, Return and Refund Policy</p>
-            <p>Terms and Conditions</p>
-        </div>
+        <div className=" flex w-full justify-end gap-3">
+          <div class="flex flex-col gap-[0vh] w-48">
+              <p>FAQs</p>
+              <p>Privacy Policy</p>
+              <p>Pricing, Delivery, Return </p>
+              <p>Refund Policy</p>
+              <p>Terms and Conditions</p>
+          </div>
 
-        <div class="flex flex-col gap-[0vh]">
-            <p>Contact Us</p>
-            <p>About Us</p>
-            <p>Pickup Points</p>
-            <p>Disclaimer</p>
+          <div class="flex flex-col gap-[0vh]">
+              <p>Contact Us</p>
+              <p>About Us</p>
+              <p>Pickup Points</p>
+              <p>Disclaimer</p>
+          </div>
         </div>
     </div>
 
-    <hr class="w-[90%] border-[0.03vh] border-[#000000] mx-auto my-[2vh]"/>
+    <hr class="w-full border-[0.03vh] border-[#000000] mx-auto my-[2vh]"/>
     <div class="text-center">&copy; 2024 Avenue E-Commerce Limited(AEL) All Rights Reserved.</div>
   </div>
     </>
