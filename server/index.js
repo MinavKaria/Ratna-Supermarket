460b784f34a53ea94de209bd048d97e8d00e541a
MinavKaria <minavpkaria@gmail.com>
2024-04-15 00:41:38 +0530

    Add CORS headers to server/index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ import Order from './cartOrderSchema.js';
 const app = express();
 dotenv.config();
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
