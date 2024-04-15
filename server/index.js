8abbbcb59c0802e9a632bd3ba51d603571e7df8d
MinavKaria <minavpkaria@gmail.com>
2024-04-15 18:30:28 +0530

    fix cors error

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config();
 app.use(cors({
   origin: ['*', 'http://localhost:5173'],
-  methods: 'GET,POST,PUT',
+  methods: ['GET,POST,PUT'],
   allowedHeaders: 'Content-Type,Authorization',
 }));
 
