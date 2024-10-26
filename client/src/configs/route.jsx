59d213172728faf65b5d730d61d1f7879d6de197
shiven <shivenyadavs@gmail.com>
2024-10-26 17:30:33 +0530

    Landing page responsive

--- client/src/configs/route.jsx
+++ client/src/configs/route.jsx
@@ -1,11 +1,6 @@
 import Layout from "../pages/Layout";
 import Home from "../pages/Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Signup from "../pages/Signup";
@@ -16,7 +11,7 @@ import CategoryList from "../components/CategoryList";
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
 import Tracking from "../components/Tracking";
-import Vender from "../pages/Vendor";
+import Vendor from "../pages/Vendor";
 import Orders from "../pages/Orders";
 import ProductPage from "../pages/ProductPage";
 import VendorOrders from "../pages/VendorOrders";
@@ -24,135 +19,28 @@ import VendorProducts from "../pages/VendorProducts";
 import Feedback from "../pages/Feedback";
 import VendorFeedback from "../pages/VendorFeedback";
 
+// Router Configuration
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      {
-        path: "/",
-        element: (
-          <>
-       
-            <Home />
-     
-          </>
-        ),
-      },
-      {
-        path: "/sign",
-        element: (
-          <>
-            <Signup />
-          </>
-        ),
-      },
-      {
-        path: "/categories/:categoryName",
-        element: (
-          <>
-            <CategoryList/>
-          </>
-        )
-      },{
-        path: "/cart",
-        element: (
-          <>
-            
-            <Cart />
-            
-          </>
-        ),
-      },{
-        path: "/checkout",
-        element: (
-          <>
-          
-            <Checkout/>
-            
-          </>
-        ),
-      },
-      {
-        path:'/tracking/:id',
-        element: (
-          <>
-     
-            <div className="">
-              <Tracking/>
-            </div>
-         
-          </>
-        )
-      },
-      {
-        path: '/orders',
-        element: (
-          <>
-            <Orders/>
-          </>
-        )
-      },
-      {
-        path: "/vendor",
-        element: (
-          <>
-            <Vender/>
-          </>
-        ),
-
-      },{
-        path:'/product/:id',
-        element:
-        (
-        <>
-          <ProductPage/>
-        </>
-        ),
-      },{
-        path:'/vendor/orders',
-        element:(
-          <>
-            <VendorOrders/>
-          </>
-        )
-      },
-      {
-        path: '/vendor/products',
-        element: (
-          <>
-            <VendorProducts/>
-          </>
-        )
-      },
-      {
-        path: 'feedback/:id',
-        element: (
-          <>
-            <Feedback/>
-          </>
-        )
-      },
-      {
-        path:"vendor/feedback",
-        element:(
-          <>
-            <VendorFeedback/>
-          </>
-        )
-      },
-      {
-        path: "*",
-        element: (
-          <>
-             <NotFound/>
-          </>
-        ),
-      },
+      { path: "/", element: <Home /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/categories/:categoryName", element: <CategoryList /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/checkout", element: <Checkout /> },
+      { path: "/tracking/:id", element: <Tracking /> },
+      { path: "/orders", element: <Orders /> },
+      { path: "/vendor", element: <Vendor /> },
+      { path: "/product/:id", element: <ProductPage /> },
+      { path: "/vendor/orders", element: <VendorOrders /> },
+      { path: "/vendor/products", element: <VendorProducts /> },
+      { path: "/feedback/:id", element: <Feedback /> },
+      { path: "/vendor/feedback", element: <VendorFeedback /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
-
-
 ]);
 
 export default router;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import Carousel from '../components/Carousel'
-import CategoriesHead from '../components/TopCategoriesHead'
-import TopCategoriesList from '../components/TopCategoriesList'
-import BuyCard from '../components/BuyCards'
-import ChatbotHelp from '../components/ChatbotHelp'
+import React from 'react';
+import Carousel from '../components/Carousel';
+import CategoriesHead from '../components/TopCategoriesHead';
+import TopCategoriesList from '../components/TopCategoriesList';
+import BuyCard from '../components/BuyCards';
+import ChatbotHelp from '../components/ChatbotHelp';
 
 function Home() {
   return (
     <>
-      <div>
-        <div className='md:mt-24 sm:mt-36 '>
-          <Carousel/>
-        </div>
-        <CategoriesHead title="Shop From" greenTitle="Top Categories"/>
-        <TopCategoriesList/>
-        <CategoriesHead title="Supersaver" greenTitle="Up to 50% off"/>
-        <BuyCard/>
-      </div>
+      <section className='md:mt-24 sm:mt-36'>
+        {/* Carousel for displaying featured products or offers */}
+        <Carousel />
+      </section>
+      
+      {/* Top Categories Section */}
+      <section>
+        <CategoriesHead 
+          title="Shop From" 
+          greenTitle="Top Categories" 
+          href="client/src/components/GoogleLoginButton.jsx" 
+        />
+        <TopCategoriesList />
+      </section>
+
+      {/* Supersaver Offers Section */}
+      <section>
+        <CategoriesHead 
+          title="Supersaver" 
+          greenTitle="Up to 50% off" 
+          href="client/src/components/TopCategoriesList.jsx" 
+        />
+        <BuyCard />
+      </section>
+
+      {/* Chatbot Help */}
       <ChatbotHelp />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
